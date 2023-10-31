b50596ef519b959397a61a2ec87cf0bf81f95de5
Alain Jason Bundalian <ajbundalian3@gmail.com>
2023-10-31 15:02:08 +0800

    Changed Sign Up Page

--- js/singup.js
+++ js/singup.js
@@ -1,21 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.querySelector("form");
-  
-  form.addEventListener("submit", function(event) {
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      
-      if (!isValidEmail(email)) {
-          alert("Please enter a valid email address.");
-          event.preventDefault();
-      } else if (password.trim() === "") {
-          alert("Password cannot be blank.");
-          event.preventDefault();
-      }
-  });
-  
-  function isValidEmail(email) {
-      const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-      return re.test(email);
-  }
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    const email = document.querySelector("input[name='email']").value;
+    const password = document.querySelector("input[name='password']").value;
+
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
+        alert("Please enter a valid email address!");
+        event.preventDefault();
+    }
+
+    if (password.length < 8) {
+        alert("Password should be at least 8 characters long!");
+        event.preventDefault();
+    }
 });
