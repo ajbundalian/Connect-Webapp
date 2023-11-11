3d25ad7a98d1590b568c65dbbb0687db0e905ce9
Alain Jason Bundalian <ajbundalian3@gmail.com>
2023-11-12 02:13:10 +0800

    Added Applicant Homepage

--- php/applicant-homepage.php
+++ php/applicant-homepage.php
@@ -11,6 +11,86 @@
 </head>
 <body>
 <div class="mother-ship">
+
+<?php
+session_start();
+
+// Redirect to login if not logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Database connection
+// 
+    $host = 'localhost';
+    $db   = 'connect';
+    $user = 'root';
+    $pass = '12345';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false, 
+    ];
+
+$user_id = $_SESSION['user_id'];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+// Fetch the three latest companies
+$stmt = $pdo->query("
+    SELECT company_id, company_name, company_pic
+    FROM company_profile 
+    ORDER BY company_id DESC 
+    LIMIT 3
+");
+$companies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Fetch user information
+$pdo = new PDO($dsn, $user, $pass, $options);
+$stmt = $pdo->prepare("SELECT * FROM profile WHERE user_id = :user_id");
+$stmt->bindParam(':user_id', $user_id);
+$stmt->execute();
+$userInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Fetch the three latest jobs
+$stmt = $pdo->query("
+    SELECT j.job_id, j.job_title, cp.company_name
+    FROM jobs j
+    JOIN company_profile cp ON j.user_id = cp.user_id
+    ORDER BY j.date_posted DESC
+    LIMIT 3
+");
+$jobs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($jobs as $index => $job) {
+    // Fetch the associated disabilities for each job
+    $disabilityStmt = $pdo->prepare("
+        SELECT d.disability_name
+        FROM job_disability_junction jdj
+        JOIN disability d ON jdj.disability_id = d.disability_id
+        WHERE jdj.job_id = :job_id
+    ");
+    $disabilityStmt->bindParam(':job_id', $job['job_id']);
+    $disabilityStmt->execute();
+    $disabilities = $disabilityStmt->fetchAll(PDO::FETCH_ASSOC);
+
+     // Limit the disabilities to the first two entries
+     $limitedDisabilities = array_slice($disabilities, 0, 2);
+
+    // Add the disabilities to the job array
+    $jobs[$index]['disabilities'] = $disabilities;
+}
+
+
+// Use $userInfo to populate the HTML template
+?>
+
+
 <!--Navigation Starts Here Here-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-2">
         <div class="container-fluid">
@@ -46,6 +126,7 @@
         </div>
         </nav>
 <!--Navigation Ends Here-->
+
         <div class="main" id="maincontent" style="margin-top: 10px;">
             <div class="container">
                 <aside class="profile-sidebar">
@@ -53,8 +134,8 @@
                         <div class="profile-image-wrapper">
                             <div class="profile-image"></div>
                         </div>
-                        <p class="profile-text">Welcome, </p>
-                        <p class="profile-text">District, City</p>
+                        <p class="profile-text">Welcome, <?php echo htmlspecialchars($userInfo['first_name']); ?>! </p>
+                        <p class="profile-text"> <?php echo htmlspecialchars($userInfo['district']); ?>, <?php echo htmlspecialchars($userInfo['city']); ?> </p>
                     </div>
                     <div class="edit-profile-button-container">
                         <a href="#" class="edit-profile-button">
@@ -63,9 +144,9 @@
                         </a>
                     </div>
                     <div class="profile-links">
-                        <a href=""><ion-icon name="briefcase-outline" aria-hidden="true"></ion-icon> Recommended Jobs</a>
+                        <a href="recommended-jobs.php"><ion-icon name="briefcase-outline" aria-hidden="true"></ion-icon> Recommended Jobs</a>
                         <a href=""><ion-icon name="archive-outline" aria-hidden="true"></ion-icon> Connected Jobs</a>
-                        <a href=""><ion-icon name="bookmark-outline" aria-hidden="true"></ion-icon> Saved Jobs</a>
+                        <a href="saved-jobs.php"><ion-icon name="bookmark-outline" aria-hidden="true"></ion-icon> Saved Jobs</a>
                     </div>
                 </aside>
             
@@ -76,112 +157,56 @@
                     <a href="#" class="view-all">View All</a>
                 </div>
                 <div class="recently-jobs">
-                    <div class="recently-jobs-container">
-                        <div class="recently-job-card">
-                            <article class="recently-job-content">
-                                <a class="job-card" href="#">
-                                    <div class="company-picture-container">   
-                                    <img class="company-picture" src="/image/Connect Favicon (2).svg">
-                                </div>
-                                <p class="job-title">Financial Services Representative - Forth Worth, TX at Fidelity Investments</p>
-                                <div class="company-name-container">
-                                    <p class="company-name">Fidelity Investments</p>
-                                </div>
-                                <div class="disability-name-container">
-                                    <p class="disability-name">Blindness</p>
-                                </div>
-                            </a>
-
-
-                            </article>
-                        </div>
-                        <div class="recently-job-card">
-                            <article class="recently-job-content">
-                                <a class="job-card" href="#">
-                                    <div class="company-picture-container">   
-                                    <img class="company-picture" src="/image/Connect Favicon (2).svg">
-                                </div>
-                                <p class="job-title">Financial Services Representative - Forth Worth, TX at Fidelity Investments</p>
-                                <div class="company-name-container">
-                                    <p class="company-name">Fidelity Investments</p>
-                                </div>
-                                <div class="disability-name-container">
-                                    <p class="disability-name">Blindness</p>
-                                </div>
-                            </a>
-
-
-                            </article>
-                        </div>
-                        <div class="recently-job-card">
-                            <article class="recently-job-content">
-                                <a class="job-card" href="#">
-                                    <div class="company-picture-container">   
-                                    <img class="company-picture" src="/image/Connect Favicon (2).svg">
-                                </div>
-                                <p class="job-title">Financial Services Representative - Forth Worth, TX at Fidelity Investments</p>
-                                <div class="company-name-container">
-                                    <p class="company-name">Fidelity Investments</p>
-                                </div>
-                                <div class="disability-name-container">
-                                    <p class="disability-name">Blindness</p>
-                                </div>
-                            </a>
-
-
-                            </article>
-                        </div>
+    <div class="recently-jobs-container">
+        <?php foreach ($jobs as $job): ?>
+        <div class="recently-job-card">
+            <article class="recently-job-content">
+                <a class="job-card" href="job-listing.php?job_id=<?php echo $job['job_id']; ?>">
+                    <div class="company-picture-container">   
+                        <img class="company-picture" src="/image/Connect Favicon (2).svg">
                     </div>
-                </div>
+                    <p class="job-title"><?php echo htmlspecialchars($job['job_title']); ?> at <?php echo htmlspecialchars($job['company_name']); ?></p>
+                    <div class="company-name-container">
+                        <p class="company-name"><?php echo htmlspecialchars($job['company_name']); ?></p>
+                    </div>
+                    <div class="disability-name-container">
+                    <?php if (!empty($job['disabilities'])): ?>
+                        <?php foreach ($job['disabilities'] as $disability): ?>
+                            <p class="disability-name"><?php echo htmlspecialchars($disability['disability_name']); ?></p>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    </div>
+                </a>
+            </article>
+        </div>
+        <?php endforeach; ?>
+        </div>
+        </div>
                 <div>
                     <div class="recently-company-tag">
                         <h2 class="c-tag">Recently Added Companies</h2>
-                        <a href="#" class="view-all">View All</a>
+                        <a href="company-directory.php" class="view-all">View All</a>
                     </div>
                 </div>
                 <div class="recently-company">
                     <div class="recently-company-container">
-                        <div class="recently-company-card">
-                            <article class="recently-company-content">
-                                <a href="#" class="company-card">
-                                    <div class="company-picture-container-parent">
-                                        <div class="company-picture-container2">
-                                            <img src="/image/Connect Favicon (2).svg" alt="" class="company-picture2">
-                                        </div>
+                    <?php foreach ($companies as $company): ?>
+            <div class="recently-company-card">
+                <article class="recently-company-content">
+                    <a href="#" class="company-card">
+                        <div class="company-picture-container-parent">
+                            <div class="company-picture-container2">
+                                <!-- Assuming company_logo contains the path to the logo image -->
+                                <img src="/path/to/logos/<?php echo htmlspecialchars($company['company_pic']); ?>" alt="" class="company-picture2">
                                     </div>
-                                    <div class="company-name-container2">
-                                        <p class="company-name2">Fidelity Investments</p>
-                                    </div>
+                                </div>
+                                <div class="company-name-container2">
+                            <p class="company-name2"><?php echo htmlspecialchars($company['company_name']); ?></p>
+                                </div>
                                 </a>
                             </article>
-                        </div>
-                        <div class="recently-company-card">
-                            <article class="recently-company-content">
-                                <a href="#" class="company-card">
-                                    <div class="company-picture-container-parent">
-                                        <div class="company-picture-container2">
-                                            <img src="/image/Connect Favicon (2).svg" alt="" class="company-picture2">
-                                        </div>
-                                    </div>
-                                    <div class="company-name-container2">
-                                        <p class="company-name2">Fidelity Investments</p>
-                                    </div>
-                                </a>
-                            </article>
-                        </div>
-                        <div class="recently-company-card">
-                            <article class="recently-company-content">
-                                <a href="#" class="company-card">
-                                    <div class="company-picture-container-parent">
-                                        <div class="company-picture-container2">
-                                            <img src="/image/Connect Favicon (2).svg" alt="" class="company-picture2">
-                                        </div>
-                                    </div>
-                                    <div class="company-name-container2">
-                                        <p class="company-name2">Fidelity Investments</p>
-                                    </div>
-                                </a>
-                            </article>
+                            </div>
+                        <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
@@ -200,4 +225,5 @@
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </html>
